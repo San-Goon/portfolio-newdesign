819b6d6c3af2e0e461d86de525575aa1564b11d8
San-Goon <tigerlove95@naver.com>
2022-11-11 01:19:22 +0900

    chore: add html title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>{/*여기에 폰트넣기*/}</Head>
+      <Head>
+        <title>조강훈 포트폴리오</title>
+      </Head>
       <body className="bg-gradient-to-r from-primary to-blue-400 dark:from-dark-500 dark:to-dark-700 dark:text-white">
         <Main />
         <NextScript />
