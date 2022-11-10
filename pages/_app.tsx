c35bafef04765b2e945419fc8c5db9f7f2781504
San-Goon <tigerlove95@naver.com>
2022-11-10 18:47:30 +0900

    chore: designate reponsive style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:px-48">
+    <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:px-48 sm:px-20 md:px-32">
       <div className="col-span-12 p-4 text-center bg-white rounded-2xl lg:col-span-3">
         <Sidebar />
       </div>
