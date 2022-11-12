9318b56ff733126a84a69439b29d61d6a9e21ef1
San-Goon <tigerlove95@naver.com>
2022-11-12 16:08:29 +0900

    chore: add animation in resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,11 +1,13 @@
 import { experiencedSkills, mainSkills } from '../data';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { fadeInUp } from '../animation';
 
 const resume = () => {
   return (
     <div className="px-6 py-2">
       <div className="grid gap-6 md:grid-cols-2">
-        <div>
+        <motion.div variants={fadeInUp} initial="initial" animate="animate">
           <h5 className="my-3 text-2xl font-bold">Education</h5>
           <div>
             <h5 className="my-2 text-xl font-bold">안양대학교 (2014~2021)</h5>
@@ -14,8 +16,8 @@ const resume = () => {
           <div>
             <h5 className="my-2 text-xl font-bold">독산고등학교 ( ~2014)</h5>
           </div>
-        </div>
-        <div>
+        </motion.div>
+        <motion.div variants={fadeInUp} initial="initial" animate="animate">
           <h5 className="my-3 text-2xl font-bold">Experience</h5>
           <div>
             <h5 className="my-2 text-xl font-bold">
@@ -29,7 +31,7 @@ const resume = () => {
             </h5>
             <p className="mx-3">개발팀 / 인턴</p>
           </div>
-        </div>
+        </motion.div>
       </div>
       <div className="grid gap-6 md:grid-cols-2 mt-6">
         <div>
