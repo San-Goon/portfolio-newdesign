7189616fe4dd2cab83bd4a66eec82af3054a6fda
San-Goon <tigerlove95@naver.com>
2022-11-12 16:17:20 +0900

    chore: add animation in Projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,10 +1,17 @@
 import { projects } from '../data';
 import ProjectCard from '../components/ProjectCard';
+import { motion } from 'framer-motion';
+import { fadeInUp, stagger } from '../animation';
 
 const Projects = () => {
   return (
     <div className="px-5 py-2 overflow-y-scroll" style={{ height: '65vh' }}>
-      <div className="grid grid-cols-12 gap-4 my-3">
+      <motion.div
+        className="grid grid-cols-12 gap-4 my-3"
+        variants={stagger}
+        initial="initial"
+        animate="animate"
+      >
         {projects.map(
           ({
             image_path,
@@ -15,9 +22,10 @@ const Projects = () => {
             name,
             // description,
           }) => (
-            <div
+            <motion.div
               className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200"
               key={name}
+              variants={fadeInUp}
             >
               <ProjectCard
                 name={name}
@@ -28,10 +36,10 @@ const Projects = () => {
                 github_url={github_url}
                 // key_techs={key_techs}
               />
-            </div>
+            </motion.div>
           ),
         )}
-      </div>
+      </motion.div>
     </div>
   );
 };
