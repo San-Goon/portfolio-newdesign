cb96f624169d306d361f2afa2292886a23e63e2e
San-Goon <tigerlove95@naver.com>
2022-11-12 19:33:57 +0900

    chore: add icon in services

--- data.ts
+++ data.ts
@@ -1,42 +1,38 @@
 import { ProjectType, ServiceType, SkillType } from './type';
+import { TbAtom, TbHeartHandshake } from 'react-icons/tb';
+import { GoBrowser, GoCommentDiscussion, GoDashboard } from 'react-icons/go';
 
 // <b>태그 사용 가능
 export const services: ServiceType[] = [
   {
-    title: 'SSR 환경을 구축해보았습니다.',
+    title: 'Server Side Rendering',
     about:
       '&nbsp;성능면에서 CSR, SSG, SSR 의 차이점을 명확하게 구분할 수 있고, SEO 의 최적화를 위해 Next.js를 이용한 SSR 구현 방법으로 프로젝트를 진행해 본 경험이 있습니다.',
-    Icon: 'Icon',
+    Icon: GoBrowser,
   },
   {
-    title: 'Github 을 이용한 협업 경험이 많습니다.',
+    title: 'Collaboration',
     about:
-      '&nbsp;회사에 입사했을 때 부터 Github 을 이용해 협업을 해왔고, 그 경험을 토대로 git-flow 에 대한 이해도가 높습니다.',
-    Icon: 'Icon',
+      '&nbsp;· <b>Git-flow</b> 를 이용한 협업에 자신이 있습니다.<br />&nbsp;· <b>Restful API</b> 를 이용한 Backend 개발자 분과의 협업 경험이 있습니다.<br />&nbsp;· <b>Figma</b> 를 이용한 디자이너 분과의 협업 경험이 있습니다.',
+    Icon: TbHeartHandshake,
   },
   {
-    title: 'Backend 개발자 분과도 협업을 해보았습니다.',
-    about:
-      '&nbsp;라스트일마일 / 똑똑한개발자를 다니며 백엔드 개발자 분과 협업을 했습니다. 협업을 하며 Restful API 를 많이 연동 해 보았고, 백엔드 분과의 소통 방법을 배웠습니다.',
-    Icon: 'Icon',
-  },
-  {
-    title: '성능 최적화에 관심이 많고 시도해보았습니다.',
+    title: 'Optimization',
     about:
       '&nbsp;React 의 리렌더링 최적화를 하는 방법 및 Webpack 을 이용해서 번들 사이즈를 줄이는 방법으로 성능 최적화를 진행한 경험이 있습니다.',
-    Icon: 'Icon',
+    Icon: GoDashboard,
   },
   {
-    title: 'Atomic design 을 이해하고 있습니다.',
+    title: 'Atomic Design',
     about:
       '&nbsp;개발의 효율성을 높이기 위해 재사용 가능성을 고려한 디자인 패턴인 Atomic design 을 사용해 개발해 본 경험이 있습니다.',
-    Icon: 'Icon',
+    Icon: TbAtom,
   },
   {
-    title: ' 고객 분들과의 소통에 자신이 있습니다.',
+    title: 'Communication',
     about:
-      '&nbsp;실제 배포중인 프로그램 개발에 참여한 경험이 있으며, 그 과정에서 많은 고객 분들과 소통하면서 CS 처리를 해본 경험이 있습니다.',
-    Icon: 'Icon',
+      '&nbsp;· 실제 배포중인 프로그램 개발에 참여한 경험이 있으며, 그 과정에서 많은 고객 분들과 소통하면서 CS 처리를 해본 경험이 있습니다.<br />&nbsp;· 협업을 하며 Co-Worker 분들과의 효율적인 협업을 위한 소통 방법을 길렀습니다.',
+    Icon: GoCommentDiscussion,
   },
 ];
 
